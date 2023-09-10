067147ec26f4ed7d062b6214f155b5a85270520e
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-09-10 23:59:00 +0900

    Time Picker ios, android 설정

--- src/setting/NotificationScreen.tsx
+++ src/setting/NotificationScreen.tsx
@@ -1,11 +1,28 @@
 import React, { ReactElement } from 'react';
-import { StyleSheet, View, ViewStyle } from 'react-native';
+import {
+  Platform,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextStyle,
+  View,
+  ViewStyle,
+} from 'react-native';
 import DateButton from './DateButton';
+import DateTimePicker, {
+  DateTimePickerEvent,
+} from '@react-native-community/datetimepicker';
 import { WeekDay } from '../interface/WeekDay';
 
 function NotificationScreen(): ReactElement {
   const [selectedWeekDay, setSelectedWeekDay] = React.useState<WeekDay>('월');
+  const [date, setDate] = React.useState(new Date());
+  const [show, setShow] = React.useState(false);
   const weekdays: readonly WeekDay[] = ['월', '화', '수', '목', '금'] as const;
+  const onChange = (event: DateTimePickerEvent, date?: Date) => {
+    setDate(date || new Date());
+  };
+  const onPress = () => setShow(true);
   const onDateButtonPressed = (weekDay: WeekDay) => setSelectedWeekDay(weekDay);
   return (
     <View style={styles.container}>
@@ -19,7 +36,35 @@ function NotificationScreen(): ReactElement {
           />
         ))}
       </View>
-      <View style={styles.timeContainer} />
+      <View style={styles.timeContainer}>
+        <Pressable hitSlop={20} onPress={onPress}>
+          <Text style={styles.buttonText}>{'알림 시간 설정'}</Text>
+        </Pressable>
+        {show && Platform.OS === 'ios' && (
+          <DateTimePicker
+            onChange={onChange}
+            is24Hour={true}
+            locale={'ko-KR'}
+            mode={'time'}
+            value={date}
+          />
+        )}
+      </View>
+      {show && Platform.OS === 'android' && (
+        <View
+          style={{
+            backgroundColor: 'black',
+            opacity: 0.8,
+            ...StyleSheet.absoluteFillObject,
+          }}>
+          <DateTimePicker
+            onChange={onChange}
+            is24Hour={true}
+            mode={'time'}
+            value={date}
+          />
+        </View>
+      )}
     </View>
   );
 }
@@ -28,6 +73,7 @@ type Style = {
   container: ViewStyle;
   weekDayContainer: ViewStyle;
   timeContainer: ViewStyle;
+  buttonText: TextStyle;
 };
 
 const styles = StyleSheet.create<Style>({
@@ -43,10 +89,13 @@ const styles = StyleSheet.create<Style>({
     gap: 20,
   },
   timeContainer: {
-    backgroundColor: 'yellow',
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  buttonText: {
+    color: 'black',
+    fontSize: 40,
   },
 });
 
