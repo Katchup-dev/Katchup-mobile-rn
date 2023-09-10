9205df6b43d6e12f28891ee535ecfaa8725de32f
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-09-10 09:41:33 +0900

    remove inline style

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,35 @@
-import React from 'react';
-import {SafeAreaView, StatusBar, useColorScheme, View} from 'react-native';
+import React, {ReactElement} from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  useColorScheme,
+  View,
+} from 'react-native';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {NavigationContainer} from '@react-navigation/native';
 
 const queryClient = new QueryClient();
 
-function App(): JSX.Element {
+function App(): ReactElement {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
     <NavigationContainer>
       <QueryClientProvider client={queryClient}>
-        <SafeAreaView style={{flex: 1}}>
+        <SafeAreaView style={styles.container}>
           <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-          <View style={{flex: 1}} />
+          <View style={styles.container} />
         </SafeAreaView>
       </QueryClientProvider>
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default App;
